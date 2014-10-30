481222b3c18288399f3d88ab24857d43ba01cda5
Paul <paul.doyle@dit.ie>
2014-10-30 15:47:44 +0000

    app.py new functions

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -2,6 +2,15 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
+@app.route("//")
+def hello():
+    return "Hello Okeanus!"
+  
+@app.route(".")
+def hello():
+    return "Hello Everyone!"
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
